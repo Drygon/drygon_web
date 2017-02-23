070c8f1e79ba11cf9e991aa8450b894c041fd898
JJZi <huijieyep@shaw.ca>
2017-02-23 00:23:39 -0700

    added quote

--- src/containers/HomeBody.js
+++ src/containers/HomeBody.js
@@ -3,25 +3,21 @@ import { Grid, Segment } from 'semantic-ui-react'
 import { PhotoPage, QuotationPage } from '../components'
 
 const HomeBody = () => (
-<div id="main" className="container">
-      <section className="box features">
-        <h2 className="major"><span>Something here!!!</span></h2>
-        <div>
-          <Grid stackable columns={2}>
-            <Grid.Column>
-              <Segment>
-                <PhotoPage />
-              </Segment>
-            </Grid.Column>
-            <Grid.Column>
-              <Segment>
-                <QuotationPage />
-              </Segment>
-            </Grid.Column>
-          </Grid>
-        </div>
-      </section>
-</div>
+  <div id="main" className="container">
+    <section className="box features">
+      <h2 className="major"><span>Something here!!!</span></h2>
+      <div>
+        <Grid stackable columns="equal" stretched>
+          <Grid.Column>
+            <PhotoPage />
+          </Grid.Column>
+          <Grid.Column>
+            <QuotationPage />
+          </Grid.Column>
+        </Grid>
+      </div>
+    </section>
+  </div>
 )
 
 export default HomeBody
